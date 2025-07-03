ef3c4a0900c4c72a768fbe335a247d9eb840e370
yana <yanache51@gmail.com>
2025-07-03 17:01:58 +0300

    add ffmpeg

---
+++ audio-joiner.py
@@ -0,0 +1,16 @@
+
+import os
+
+def join_audio(audio1, audio2):
+    # we need to run ffmpeg and pass this path as an argument
+    ffmpeg_path = "/opt/homebrew/bin/ffmpeg"
+    # run test command to check if ffmpeg is installed
+    if not os.path.exists(ffmpeg_path):
+        print("ffmpeg is not installed. Please install it and try again.")
+        return
+
+    # run ffmpeg command to join the two audio files
+    os.system(f"{ffmpeg_path}")
+
+
+join_audio("system.wav", "microphone.wav")
--- translation.py
+++ translation.py
@@ -1,7 +1,8 @@
+import os
 import whisper
 
 model = whisper.load_model("turbo")
-result = model.transcribe("/Users/murmur/Documents/python/project1/myrecording.wav",fp16=False) 
+result = model.transcribe(os.path.join(__file__, 'input.wav'),fp16=False) 
 
 with open("./translate.txt", "w") as f:
     text = str(result.get('text') )
